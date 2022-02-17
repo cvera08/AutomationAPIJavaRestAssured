617d6466538ef0c62e48d7ea08fe84238f8dd386
cvera <cvera08@gmail.com>
2022-02-16 21:29:03 -0300

    Supporting testname for better logs / print failures with more details

--- src/test/java/MainPackage/BaseApiTest.java
+++ src/test/java/MainPackage/BaseApiTest.java
@@ -1,10 +1,15 @@
 package MainPackage;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 import static io.restassured.RestAssured.baseURI;
 
 public class BaseApiTest {
+
+    public String testName;
 
     public static String responseBodyPath = System.getProperty("user.dir") + "/src/test/java/Resources/JsonResponses/";
     public static String requestBodyPath = System.getProperty("user.dir") + "/src/test/java/Resources/JsonRequests/";
@@ -12,5 +17,10 @@ public class BaseApiTest {
     @BeforeSuite
     public void setup() {
         baseURI = "https://reqres.in/api/";
+    }
+
+    @BeforeMethod
+    public void handleTestMethodName(Method method) {
+        testName = method.getName();
     }
 }
